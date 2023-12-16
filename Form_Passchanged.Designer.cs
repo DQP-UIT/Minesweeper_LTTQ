42fd0ff102ab0dd3e8c730bb1eb5901c67c24ac7
DQP-UIT <153169422+DQP-UIT@users.noreply.github.com>
2023-12-16 13:59:18 +0700

    Form_Passchanged.Designer

---
+++ Form_Passchanged.Designer.cs
@@ -0,0 +1,211 @@
+namespace Y2_MineLand
+{
+    partial class Form_PasswordChanged
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.kryptonTextBox1 = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.kryptonTextBox2 = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.kryptonTextBox3 = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.lbl_OldPass = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.lbl_Newpass = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.lbl_NewpassConfirm = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.btn_Confirm = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.btn_Cancel = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kryptonPalette1 = new ComponentFactory.Krypton.Toolkit.KryptonPalette(this.components);
+            this.kryptonPanel1 = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
+            this.kryptonPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kryptonTextBox1
+            // 
+            this.kryptonTextBox1.AcceptsReturn = true;
+            this.kryptonTextBox1.Location = new System.Drawing.Point(407, 178);
+            this.kryptonTextBox1.Name = "kryptonTextBox1";
+            this.kryptonTextBox1.PasswordChar = '*';
+            this.kryptonTextBox1.Size = new System.Drawing.Size(250, 40);
+            this.kryptonTextBox1.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonTextBox1.StateCommon.Border.Rounding = 10;
+            this.kryptonTextBox1.StateCommon.Content.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonTextBox1.TabIndex = 0;
+            // 
+            // kryptonTextBox2
+            // 
+            this.kryptonTextBox2.AcceptsReturn = true;
+            this.kryptonTextBox2.Location = new System.Drawing.Point(407, 270);
+            this.kryptonTextBox2.Name = "kryptonTextBox2";
+            this.kryptonTextBox2.PasswordChar = '*';
+            this.kryptonTextBox2.Size = new System.Drawing.Size(250, 40);
+            this.kryptonTextBox2.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonTextBox2.StateCommon.Border.Rounding = 10;
+            this.kryptonTextBox2.StateCommon.Content.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonTextBox2.TabIndex = 1;
+            // 
+            // kryptonTextBox3
+            // 
+            this.kryptonTextBox3.AcceptsReturn = true;
+            this.kryptonTextBox3.Location = new System.Drawing.Point(407, 372);
+            this.kryptonTextBox3.Name = "kryptonTextBox3";
+            this.kryptonTextBox3.PasswordChar = '*';
+            this.kryptonTextBox3.Size = new System.Drawing.Size(250, 40);
+            this.kryptonTextBox3.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonTextBox3.StateCommon.Border.Rounding = 10;
+            this.kryptonTextBox3.StateCommon.Content.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonTextBox3.TabIndex = 2;
+            // 
+            // lbl_OldPass
+            // 
+            this.lbl_OldPass.AutoSize = false;
+            this.lbl_OldPass.Location = new System.Drawing.Point(407, 132);
+            this.lbl_OldPass.Name = "lbl_OldPass";
+            this.lbl_OldPass.Size = new System.Drawing.Size(230, 40);
+            this.lbl_OldPass.StateCommon.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_OldPass.TabIndex = 3;
+            this.lbl_OldPass.Values.Text = "Last Password";
+            // 
+            // lbl_Newpass
+            // 
+            this.lbl_Newpass.AutoSize = false;
+            this.lbl_Newpass.Location = new System.Drawing.Point(407, 224);
+            this.lbl_Newpass.Name = "lbl_Newpass";
+            this.lbl_Newpass.Size = new System.Drawing.Size(230, 40);
+            this.lbl_Newpass.StateCommon.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Newpass.TabIndex = 4;
+            this.lbl_Newpass.Values.Text = "New Password";
+            // 
+            // lbl_NewpassConfirm
+            // 
+            this.lbl_NewpassConfirm.AutoSize = false;
+            this.lbl_NewpassConfirm.Location = new System.Drawing.Point(407, 326);
+            this.lbl_NewpassConfirm.Name = "lbl_NewpassConfirm";
+            this.lbl_NewpassConfirm.Size = new System.Drawing.Size(230, 40);
+            this.lbl_NewpassConfirm.StateCommon.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_NewpassConfirm.TabIndex = 5;
+            this.lbl_NewpassConfirm.Values.Text = "Retype your new pass";
+            // 
+            // btn_Confirm
+            // 
+            this.btn_Confirm.Location = new System.Drawing.Point(407, 438);
+            this.btn_Confirm.Name = "btn_Confirm";
+            this.btn_Confirm.Palette = this.kryptonPalette1;
+            this.btn_Confirm.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
+            this.btn_Confirm.Size = new System.Drawing.Size(100, 45);
+            this.btn_Confirm.TabIndex = 6;
+            this.btn_Confirm.Values.Text = "Confirm";
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Location = new System.Drawing.Point(557, 438);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Palette = this.kryptonPalette1;
+            this.btn_Cancel.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
+            this.btn_Cancel.Size = new System.Drawing.Size(100, 45);
+            this.btn_Cancel.TabIndex = 7;
+            this.btn_Cancel.Values.Text = "Cancel";
+            // 
+            // kryptonPalette1
+            // 
+            this.kryptonPalette1.ButtonStyles.ButtonCommon.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPalette1.ButtonStyles.ButtonCommon.StateCommon.Border.Rounding = 15;
+            this.kryptonPalette1.ButtonStyles.ButtonCommon.StateCommon.Border.Width = 3;
+            this.kryptonPalette1.ButtonStyles.ButtonCommon.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // kryptonPanel1
+            // 
+            this.kryptonPanel1.Controls.Add(this.pictureBox1);
+            this.kryptonPanel1.Location = new System.Drawing.Point(-3, 0);
+            this.kryptonPanel1.Name = "kryptonPanel1";
+            this.kryptonPanel1.Size = new System.Drawing.Size(300, 525);
+            this.kryptonPanel1.StateCommon.Color1 = System.Drawing.Color.LightSteelBlue;
+            this.kryptonPanel1.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.pictureBox1.Image = global::Y2_MineLand.Properties.Resources.passchangedgif;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 105);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(297, 342);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form_PasswordChanged
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 523);
+            this.Controls.Add(this.kryptonPanel1);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.btn_Confirm);
+            this.Controls.Add(this.lbl_NewpassConfirm);
+            this.Controls.Add(this.lbl_Newpass);
+            this.Controls.Add(this.lbl_OldPass);
+            this.Controls.Add(this.kryptonTextBox3);
+            this.Controls.Add(this.kryptonTextBox2);
+            this.Controls.Add(this.kryptonTextBox1);
+            this.Name = "Form_PasswordChanged";
+            this.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.StateCommon.Border.Rounding = 20;
+            this.StateCommon.Border.Width = 2;
+            this.Text = "Form_PasswordChanged";
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).EndInit();
+            this.kryptonPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox kryptonTextBox1;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox kryptonTextBox2;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox kryptonTextBox3;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel lbl_OldPass;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel lbl_Newpass;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel lbl_NewpassConfirm;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton btn_Confirm;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton btn_Cancel;
+        private ComponentFactory.Krypton.Toolkit.KryptonPalette kryptonPalette1;
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel kryptonPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
